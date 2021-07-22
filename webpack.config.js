e44f7da152cc50afccb431ff0926da0219e76995
Radonevsky <Radonevsky@users.noreply.github.com>
2021-07-22 21:27:05 +0300

    Mini css extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -32,13 +35,27 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: './index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+      ]
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css'
+  }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {},
+        }, 'css-loader']
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
